520bb61eb67bb7d6eee4b7cd44c8c476a1ed63fe
Emik <emik.lakunishok@gmail.com>
2023-01-14 23:09:38 +0100

    Rename file, again

--- Source/ResultException`1.cs
+++ Source/ResultException`1.cs
@@ -1,6 +1,6 @@
 ﻿#region Emik.MPL
 
-// <copyright file="ResultException{T}.cs" company="Emik">
+// <copyright file="ResultException`1.cs" company="Emik">
 // Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
 
