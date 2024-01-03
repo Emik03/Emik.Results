e985e32a721496cf0b76d641eec76bdd588a9093
Emik <emik.lakunishok@gmail.com>
2024-01-03 13:38:56 +0100

    Create NamespaceDoc.cs

---
+++ NamespaceDoc.cs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MPL-2.0
+#pragma warning disable MA0048, SA1403
+#if DEBUG
+namespace Emik.Results
+{
+    /// <summary>Contains the Result type; A type representing either a success value or failure value.</summary>
+    [UsedImplicitly]
+    struct AssemblyDoc;
+
+    /// <summary>Contains the core types, including <see cref="Result{TOk, TErr}"/>.</summary>
+    [UsedImplicitly]
+    struct NamespaceDoc;
+}
+
+namespace Emik.Results.Extensions
+{
+    /// <summary>
+    /// Contains extensions for creating <see cref="Result{TOk, TErr}"/> from other types,
+    /// or operations requiring constrained generics within <see cref="Result{TOk, TErr}"/>.
+    /// </summary>
+    [UsedImplicitly]
+    struct NamespaceDoc;
+}
+#endif
